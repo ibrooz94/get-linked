cf3092e9b7c8692af07dc847a33085fc1213468f
ibrooz94 <ibrooz94@gmail.com>
2023-09-29 02:57:25 +0100

    update: create body section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -30,8 +30,7 @@ const purpleGradient = "bg-gradient-to-r from-purple to-bright-purple hover:bg-g
                 <div class="flex flex-col items-center lg:items-start flex-initial gap-4 text-center lg:text-start">
                     <h1 class="font-clash text-3xl lg:text-7xl pt-8 max-w-[300px] lg:max-w-[600px] font-bold">
                         getlinked <span class="bulb relative">Tech
-                            <span class="absolute top-[-.9em] right-[.5em] animate-bounce"
-                                style="font-size: .7em;">
+                            <span class="absolute top-[-.9em] right-[.5em] animate-bounce" style="font-size: .7em;">
                                 <img src="../assets/images/bulb.png" alt="" style="width: 1em; height: auto;">
                             </span>
                         </span> <br>
@@ -46,12 +45,11 @@ const purpleGradient = "bg-gradient-to-r from-purple to-bright-purple hover:bg-g
                     <button :class="purpleGradient" class=" text-white py-4 px-14 lg:self-start">
                         Register
                     </button>
-                    <CoundownTimer/>
+                    <CoundownTimer />
                 </div>
 
                 <div class="hero-image lg:w-[55%] -mx-[9%] relative">
-                    <img class="opacity-70 grayscale-[80%] w-full" src="../assets/images/glassesman.png"
-                        alt="">
+                    <img class="opacity-70 grayscale-[80%] w-full" src="../assets/images/glassesman.png" alt="">
                     <img class="absolute top-0 animate-spin-slow" src="../assets/images/globe.png" alt="">
 
                 </div>
@@ -59,17 +57,32 @@ const purpleGradient = "bg-gradient-to-r from-purple to-bright-purple hover:bg-g
 
         </section>
     </header>
-    <hr class="border-zinc-600 m-0">
+    <hr class="border-zinc-600 mb-14">
 
-    <section class="">
-
+    <section class="flex flex-col lg:flex-row items-center justify-between gap-8 lg:gap-[10%] w-[80%] max-w-[calc(1512px-15%)] mx-auto ">
+        <div class="pb-8 relative ">
+            <img class="absolute left-4" src="../assets/images/star_purple.png" alt="">
+            <img class="absolute w-8 lg:w-auto bottom-[-5%] lg:bottom-[1.5em] right-[45%] lg:right-0 lg:-mr-14 " src="../assets/images/curly_arrow.png" alt="">
+            <img src="../assets/images/big_idea.png" alt="">
+        </div>
+        <div class="text-white">
+            <p class="text-xl lg:text-3xl font-clash font-bold mb-1"> Introduction to getlinked <br>
+                <span class="text-purple"> techHackathon 1.0</span> </p>
+            <p class="max-w-[500px]"> Our tech hackathon is a melting pot of visionaries, and its purpose is as
+                clear as day: to shape the future. Whether you're a coding genius, a
+                design maverick, or a concept wizard, you'll have the chance to transform
+                your ideas into reality. Solving real-world problems, pushing the boundaries
+                of technology, and creating solutions that can change the world,
+                that's what we're all about!</p>
+        </div>
     </section>
 </template>
 
 <style scoped>
-.hero-image{
+.hero-image {
     animation: scaleIn 1s ease-in;
 }
+
 header {
     position: relative;
     background: url('../assets/images/metrix.png');
@@ -128,23 +141,26 @@ header::before {
     z-index: 1;
     /* Ensure the image is above the text */
 }
-@keyframes slidein{
-    0%{
-        transform: scale(0);
-    }
-    100%{
-        transform: scale(1);
 
-    }
-}
-@keyframes scaleIn{
-    0%{
+@keyframes slidein {
+    0% {
         transform: scale(0);
     }
-    100%{
+
+    100% {
         transform: scale(1);
 
     }
 }
 
+@keyframes scaleIn {
+    0% {
+        transform: scale(0);
+    }
+
+    100% {
+        transform: scale(1);
+
+    }
+}
 </style>
